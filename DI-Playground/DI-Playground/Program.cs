326e0cf905f6a04030b7dadc35442f5580ba1802
jimtete <tetepoulidis@ieee.org>
2023-01-12 13:13:32 +0200

    main-branch initialization.

---
+++ DI-Playground/DI-Playground/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DI_Playground
+{
+    public interface ILog
+    {
+        void Write(string message);
+    }
+
+    public class ConsoleLog : ILog
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class Engine
+    {
+        private ILog _log;
+        private int _id;
+
+        public Engine(ILog log)
+        {
+            _log = log;
+            _id = new Random().Next();
+        }
+
+        public void Ahead(int power)
+        {
+            _log.Write($"Engine [{_id}] ahead {power}");
+        }
+    }
+
+    public class Car
+    {
+        private Engine _engine;
+        private ILog _log;
+
+        public Car(Engine engine, ILog log)
+        {
+            _engine = engine;
+            _log = log;
+        }
+
+        public void Go()
+        {
+            _engine.Ahead(100);
+            _log.Write("Car going forward...");
+        }
+    }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var log = new ConsoleLog();
+            var engine = new Engine(log);
+            var car = new Car(engine, log);
+            car.Go();
+        }
+    }
+}
