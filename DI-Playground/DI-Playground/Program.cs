9100216050d59e7924b9930bba68344a207a0e82
jimtete <tetepoulidis@ieee.org>
2023-01-20 10:09:03 +0200

    using modules

--- DI-Playground/DI-Playground/Program.cs
+++ DI-Playground/DI-Playground/Program.cs
@@ -10,176 +10,78 @@ using Module = Autofac.Module;
 
 namespace DI_Playground
 {
-    public interface ILog
+    public interface IVehicle
     {
-        void Write(string message);
+        void Go();
     }
 
-    public interface IConsole
+    class Truck : IVehicle
     {
-        
-    }
+        private IDriver _driver;
 
-    public class ConsoleLog : ILog, IConsole, IDisposable
-    {
-        public ConsoleLog()
+        public Truck(IDriver driver)
         {
-            Console.WriteLine("Creating a console log!");
-        }
-        public void Write(string message)
-        {
-            Console.WriteLine(message);
-        }
-
-        public void Dispose()
-        {
-            Console.WriteLine("Console logger no longer required");
-        }
-    }
-
-    public class EmailLog : ILog
-    {
-        private const string _adminEmail = "admin@foo.com";
-
-        public void Write(string message)
-        {
-            Console.WriteLine($"Email sent to {_adminEmail} : {message}");
-        }
-    }
-
-    public class Engine
-    {
-        private ILog _log;
-        private int _id;
-
-        public Engine(ILog log)
-        {
-            _log = log;
-            _id = new Random().Next();
-        }
-
-        public Engine(ILog log, int id)
-        {
-            _log = log;
-            _id = id;
-        }
-
-        public void Ahead(int power)
-        {
-            _log.Write($"Engine [{_id}] ahead {power}");
-        }
-    }
-    
-    public class SMSLog : ILog
-    {
-        private string _phoneNumber;
-
-        public SMSLog(string phoneNumber)
-        {
-            _phoneNumber = phoneNumber;
-        }
-
-        public void Write(string message)
-        {
-            Console.WriteLine($"SMS to {_phoneNumber} : {message}");
-        }
-    }
-
-    public class Car
-    {
-        private Engine _engine;
-        private ILog _log;
-
-        public Car(Engine engine)
-        {
-            _engine = engine;
-            _log = new EmailLog();
-        }
-
-        public Car(Engine engine, ILog log)
-        {
-            _engine = engine;
-            _log = log;
+            if (driver == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(driver));
+            }
+            _driver = driver;
         }
 
         public void Go()
         {
-            _engine.Ahead(100);
-            _log.Write("car going forward...");
+            _driver.Drive();
         }
     }
 
-    public class Parent
+    public interface IDriver
     {
-        public override string ToString()
-        {
-            return "I am your father";
-        }
+        void Drive();
     }
 
-    public class Child
+    public class CrazyDriver : IDriver
     {
-        public string Name { get; set; }
-        public Parent Parent { get; set; }
-
-        public Child()
+        public void Drive()
         {
-            Console.WriteLine("Child being created");
-        }
-        
-        public void SetParent(Parent parent)
-        {
-            Parent = parent;
-        }
-
-        public override string ToString()
-        {
-            return "Hi there";
-        }
-    }
-
-    class BadChild : Child
-    {
-        public override string ToString()
-        {
-            return "I Hate you";
-        }
-    }
-
-    public class ParentChildModule : Module
-    {
-        protected override void Load(ContainerBuilder builder)
-        {
-            builder.RegisterType<Parent>();
-            builder.Register(c => new Child() { Parent = c.Resolve<Parent>() });
-        }
-    }
-
-    public class MyClass : IStartable
-    {
-        public MyClass()
-        {
-            Console.WriteLine("My class ctor");
-        }
-        public void Start()
-        {
-            Console.WriteLine("Container being built");
+            Console.WriteLine("Going too fast and crashing into a tree");
         }
     }
     
+    public class SaneDriver : IDriver
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Driving safely to destination");
+        }
+    }
+
+    public class TransportModule : Module
+    {
+        public bool ObeySpeedLimit { get; set; }
+        protected override void Load(ContainerBuilder builder)
+        {
+            if (ObeySpeedLimit)
+            {
+                builder.RegisterType<SaneDriver>().As<IDriver>();
+            }
+            else
+            {
+                builder.RegisterType<CrazyDriver>().As<IDriver>();
+            }
+
+            builder.RegisterType<Truck>().As<IVehicle>();
+        }
+    }
     internal class Program
     {
         public static void Main(string[] args)
         {
             var builder = new ContainerBuilder();
-            builder.RegisterType<MyClass>()
-                .AsSelf()
-                .As<IStartable>()
-                .SingleInstance();
-
-            var container = builder.Build();
-
-            container.Resolve<MyClass>();
+            builder.RegisterModule(new TransportModule{ObeySpeedLimit = true});
+            using (var c = builder.Build())
+            {
+                c.Resolve<IVehicle>().Go();
+            }
         }
     }
 }
