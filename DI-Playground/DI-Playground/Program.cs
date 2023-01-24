0fce4f4765dfa7bed05e10c630d90fee77ea894c
jimtete <tetepoulidis@ieee.org>
2023-01-24 12:57:37 +0200

    Implementing Adapters

--- DI-Playground/DI-Playground/Program.cs
+++ DI-Playground/DI-Playground/Program.cs
@@ -4,121 +4,99 @@ using Autofac.Core;
 using Autofac.Core.Activators.Delegate;
 using Autofac.Core.Lifetime;
 using Autofac.Core.Registration;
+using Autofac.Features.Metadata;
 using Autofac.Features.ResolveAnything;
 
 namespace DI_Playground
 {
-    public abstract class BaseHandler
-    {
-        public virtual string Handle(string message)
-        {
-            return "Handled: " + message;
-        }
-    }
-
-    public class HandlerA : BaseHandler
-    {
-        public override string Handle(string message)
-        {
-            return "Handled by A: " + message;
-        }
-    }
-    public class HandlerB : BaseHandler
-    {
-        public override string Handle(string message)
-        {
-            return "Handled by B: " + message;
-        }
-    }
-
-    public interface IHandlerFactory
-    {
-        T GetHandler<T>() where T : BaseHandler;
-    }
-
-    public class HandlerFactory : IHandlerFactory
-    {
-        public T GetHandler<T>() where T : BaseHandler
-        {
-            return Activator.CreateInstance<T>();
-        }
-    }
-
-    public class ConsumerA
-    {
-        private HandlerA _handlerA;
-
-        public ConsumerA(HandlerA handlerA)
-        {
-            _handlerA = handlerA;
-        }
-
-        public void DoWork()
-        {
-            Console.WriteLine(_handlerA.Handle("Consumer A"));
-        }
-    }
-    
-    public class ConsumerB
-    {
-        private HandlerB _handlerB;
-
-        public ConsumerB(HandlerB handlerB)
-        {
-            _handlerB = handlerB;
-        }
-
-        public void DoWork()
-        {
-            Console.WriteLine(_handlerB.Handle("Consumer B"));
-        }
-    }
-
-    public class HandlerRegistrationSource : IRegistrationSource
-    {
-        public IEnumerable<IComponentRegistration> RegistrationsFor(
-            Service service,
-            Func<Service,IEnumerable<ServiceRegistration>> registrationAccessor)
-        {
-            var swt = service as IServiceWithType;
-            if (swt == null || swt.ServiceType == null || !swt.ServiceType.IsAssignableTo<BaseHandler>())
-            {
-                yield break;
-            }
-
-            yield return new ComponentRegistration(
-                Guid.NewGuid(),
-                new DelegateActivator(swt.ServiceType, (c, p) =>
-                {
-                    var provider = c.Resolve<IHandlerFactory>();
-                    var method = provider.GetType().GetMethod("GetHandler").MakeGenericMethod(swt.ServiceType);
-                    return method.Invoke(provider, null);
-                }
-            ),
-                new CurrentScopeLifetime(),
-                InstanceSharing.None,
-                InstanceOwnership.OwnedByLifetimeScope,
-                new [] {service},
-                new ConcurrentDictionary<string, object?>());
-        }
-
-        public bool IsAdapterForIndividualComponents => false;
-    }
     
     public class Program
     {
+        public interface ICommand
+        {
+            void Execute();
+        }
+
+        class SaveCommand : ICommand
+        {
+            public void Execute()
+            {
+                Console.WriteLine("Saving a file");
+            }
+        }
+        
+        class OpenCommand : ICommand
+        {
+            public void Execute()
+            {
+                Console.WriteLine("Opening a file");
+            }
+        }
+        
+        public class Button
+        {
+            private ICommand _command;
+            private string _name;
+
+            public Button(ICommand command, string name)
+            {
+                _command = command;
+                _name = name;
+            }
+
+            public void Click()
+            {
+                _command.Execute();
+            }
+
+            public void PrintMe()
+            {
+                Console.WriteLine($"I am a button called {_name}");
+            }
+        }
+
+        public class Editor
+        {
+            private IEnumerable<Button> _buttons;
+
+            public IEnumerable<Button> Buttons => _buttons;
+
+            public Editor(IEnumerable<Button> buttons)
+            {
+                _buttons = buttons;
+            }
+
+            public void ClickAll()
+            {
+                foreach (var button in _buttons)
+                {
+                    button.Click();
+                }
+            }
+        }
+        
         static void Main(string[] args)
         {
             var b = new ContainerBuilder();
-            b.RegisterType<HandlerFactory>().As<IHandlerFactory>();
-            b.RegisterSource(new HandlerRegistrationSource());
-            b.RegisterType<ConsumerA>();
-            b.RegisterType<ConsumerB>();
-            
+            b.RegisterType<SaveCommand>().As<ICommand>()
+                .WithMetadata("_name","save");
+            b.RegisterType<OpenCommand>().As<ICommand>()
+                .WithMetadata("_name","open");
+            //b.RegisterType<Button>();
+            //b.RegisterAdapter<ICommand, Button>(cmd => new Button(cmd));
+            b.RegisterAdapter<Meta<ICommand>, Button>(cmd =>
+                new Button(cmd.Value, (string)cmd.Metadata["_name"]));
+            b.RegisterType<Editor>();
+
             using (var c = b.Build())
             {
-                c.Resolve<ConsumerA>().DoWork();
-                c.Resolve<ConsumerB>().DoWork();
+                var editor = c.Resolve<Editor>();
+                //editor.ClickAll();
+
+                foreach (var btn in editor.Buttons)
+                {
+                    btn.PrintMe();
+                }
             }
         }
     }
