0036de6665029feedda6ddaed200237bd7070e07
jimtete <tetepoulidis@ieee.org>
2023-01-24 10:54:11 +0200

    JSON/XML Configuration

--- DI-Playground/DI-Playground/Program.cs
+++ DI-Playground/DI-Playground/Program.cs
@@ -1,86 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
+﻿using System.Reflection;
 using Autofac;
-using Autofac.Core;
-using Autofac.Features.Indexed;
-using Autofac.Features.Metadata;
-using Autofac.Features.OwnedInstances;
-using Module = Autofac.Module;
+using Autofac.Configuration;
+using Microsoft.Extensions.Configuration;
 
 namespace DI_Playground
 {
-    public interface IVehicle
+    public interface IOperation
     {
-        void Go();
+        float Calculate(float a, float b);
     }
 
-    class Truck : IVehicle
+    public class Addition : IOperation
     {
-        private IDriver _driver;
-
-        public Truck(IDriver driver)
+        public float Calculate(float a, float b)
         {
-            if (driver == null)
+            return a + b;
+        }
+    }
+
+    public class Multiplication : IOperation
+    {
+        public float Calculate(float a, float b)
+        {
+            return a * b;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "./");
+
+            
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json");
+
+            var configuration = configBuilder.Build();
+            var containerBuilder = new ContainerBuilder();
+            var configModule = new ConfigurationModule(configuration);
+            containerBuilder.RegisterModule(configModule);
+
+            using (var container = containerBuilder.Build())
             {
-                throw new ArgumentNullException(paramName: nameof(driver));
-            }
-            _driver = driver;
-        }
+                float a = 3, b = 4;
 
-        public void Go()
-        {
-            _driver.Drive();
-        }
-    }
-
-    public interface IDriver
-    {
-        void Drive();
-    }
-
-    public class CrazyDriver : IDriver
-    {
-        public void Drive()
-        {
-            Console.WriteLine("Going too fast and crashing into a tree");
-        }
-    }
-    
-    public class SaneDriver : IDriver
-    {
-        public void Drive()
-        {
-            Console.WriteLine("Driving safely to destination");
-        }
-    }
-
-    public class TransportModule : Module
-    {
-        public bool ObeySpeedLimit { get; set; }
-        protected override void Load(ContainerBuilder builder)
-        {
-            if (ObeySpeedLimit)
-            {
-                builder.RegisterType<SaneDriver>().As<IDriver>();
-            }
-            else
-            {
-                builder.RegisterType<CrazyDriver>().As<IDriver>();
-            }
-
-            builder.RegisterType<Truck>().As<IVehicle>();
-        }
-    }
-    internal class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = new ContainerBuilder();
-            builder.RegisterModule(new TransportModule{ObeySpeedLimit = true});
-            using (var c = builder.Build())
-            {
-                c.Resolve<IVehicle>().Go();
+                foreach (IOperation op in container.Resolve<IList<IOperation>>())
+                {
+                    Console.WriteLine($"{op.GetType().Name} of {a} and {b} = {op.Calculate(a,b)}");
+                }
             }
         }
     }
