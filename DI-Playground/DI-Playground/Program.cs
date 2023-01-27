32a6b30e08f33db0cf57e82aaa5bf744e121f471
jimtete <tetepoulidis@ieee.org>
2023-01-27 10:31:42 +0200

    Decorators

--- DI-Playground/DI-Playground/Program.cs
+++ DI-Playground/DI-Playground/Program.cs
@@ -1,102 +1,49 @@
-﻿using System.Collections.Concurrent;
-using Autofac;
-using Autofac.Core;
-using Autofac.Core.Activators.Delegate;
-using Autofac.Core.Lifetime;
-using Autofac.Core.Registration;
-using Autofac.Features.Metadata;
-using Autofac.Features.ResolveAnything;
+﻿using Autofac;
 
 namespace DI_Playground
 {
-    
+    public interface IReportingService
+    {
+        void Report();
+    }
+
+    public class ReportingService : IReportingService
+    {
+        public void Report()
+        {
+            Console.WriteLine("Here's your report");
+        }
+    }
+
+    public class ReportingServiceWithLogging : IReportingService
+    {
+        private IReportingService _decorated;
+
+        public ReportingServiceWithLogging(IReportingService decorated)
+        {
+            _decorated = decorated;
+        }
+        public void Report()
+        {
+            Console.WriteLine("Commencing log...");
+            _decorated.Report();
+            Console.WriteLine("... ending log.");
+        }
+    }
     public class Program
     {
-        public interface ICommand
-        {
-            void Execute();
-        }
-
-        class SaveCommand : ICommand
-        {
-            public void Execute()
-            {
-                Console.WriteLine("Saving a file");
-            }
-        }
-        
-        class OpenCommand : ICommand
-        {
-            public void Execute()
-            {
-                Console.WriteLine("Opening a file");
-            }
-        }
-        
-        public class Button
-        {
-            private ICommand _command;
-            private string _name;
-
-            public Button(ICommand command, string name)
-            {
-                _command = command;
-                _name = name;
-            }
-
-            public void Click()
-            {
-                _command.Execute();
-            }
-
-            public void PrintMe()
-            {
-                Console.WriteLine($"I am a button called {_name}");
-            }
-        }
-
-        public class Editor
-        {
-            private IEnumerable<Button> _buttons;
-
-            public IEnumerable<Button> Buttons => _buttons;
-
-            public Editor(IEnumerable<Button> buttons)
-            {
-                _buttons = buttons;
-            }
-
-            public void ClickAll()
-            {
-                foreach (var button in _buttons)
-                {
-                    button.Click();
-                }
-            }
-        }
-        
         static void Main(string[] args)
         {
             var b = new ContainerBuilder();
-            b.RegisterType<SaveCommand>().As<ICommand>()
-                .WithMetadata("_name","save");
-            b.RegisterType<OpenCommand>().As<ICommand>()
-                .WithMetadata("_name","open");
-            //b.RegisterType<Button>();
-            //b.RegisterAdapter<ICommand, Button>(cmd => new Button(cmd));
-            b.RegisterAdapter<Meta<ICommand>, Button>(cmd =>
-                new Button(cmd.Value, (string)cmd.Metadata["_name"]));
-            b.RegisterType<Editor>();
+            b.RegisterType<ReportingService>().Named<IReportingService>("reporting");
+            b.RegisterDecorator<IReportingService>(
+                (context, service) => new ReportingServiceWithLogging(service), "reporting"
+            );
 
             using (var c = b.Build())
             {
-                var editor = c.Resolve<Editor>();
-                //editor.ClickAll();
-
-                foreach (var btn in editor.Buttons)
-                {
-                    btn.PrintMe();
-                }
+                var r = c.Resolve<IReportingService>();
+                r.Report();
             }
         }
     }
