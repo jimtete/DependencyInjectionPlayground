d2a48f315ee8c291b5b64ad5c6a7bb58190142cb
jimtete <tetepoulidis@ieee.org>
2023-01-30 10:21:54 +0200

    Aggregate Services

--- DI-Playground/DI-Playground/Program.cs
+++ DI-Playground/DI-Playground/Program.cs
@@ -1,72 +1,94 @@
 ﻿using System.ComponentModel.Composition;
 using Autofac;
+using Autofac.Extras.AggregateService;
 using Autofac.Extras.AttributeMetadata;
 using Autofac.Features.AttributeFilters;
 
 namespace DI_Playground
 {
-    [MetadataAttribute]
-    public class AgeMetadataAttribute : Attribute
-    {
-        public int Age { get; set; }
-
-        public AgeMetadataAttribute(int age)
-        {
-            Age = age;
-        }
-    }
-
-    public interface IArtwork
-    {
-        void Display();
-    }
-
-    [AgeMetadata(100)]
-    public class CenturyArtwork : IArtwork
-    {
-        public void Display()
-        {
-            Console.WriteLine("Displaying a century-old piece");
-        }
-    }
-
-    [AgeMetadata(1000)]
-    public class MillenialArtwork : IArtwork
-    {
-        public void Display()
-        {
-            Console.WriteLine("Displaying a really old piece of art");
-        }
-    }
-
-    public class ArtDisplay
-    {
-        private IArtwork _artwork;
-
-        public ArtDisplay([MetadataFilter("Age", 100)] IArtwork artwork)
-        {
-            _artwork = artwork;
-        }
-
-        public void Display()
-        {
-            _artwork.Display();
-        }
-    }
-    
     public class Program
     {
+        public interface IService1
+        {
+            
+        }
+        
+        public interface IService2
+        {
+            
+        }
+        
+        public interface IService3
+        {
+            
+        }
+        
+        public interface IService4
+        {
+            
+        }
+        
+        public interface IMyAggregateService
+        {
+            IService1 Service1 { get; }
+            IService2 Service2 { get; }
+            IService3 Service3 { get; }
+            /*IService4 Service4 { get; }*/
+
+            IService4 GetFourthService(string name);
+        }
+
+        public class Class1 : IService1
+        {
+            
+        }
+
+        public class Class2 : IService2
+        {
+            
+        }
+
+        public class Class3 : IService3
+        {
+            
+        }
+
+        public class Class4 : IService4
+        {
+            private string _name;
+
+            public Class4(string name)
+            {
+                _name = name;
+            }
+        }
+
+        public class Consumer
+        {
+            public IMyAggregateService AllServices;
+
+            public Consumer(IMyAggregateService allServices)
+            {
+                AllServices = allServices;
+            }
+        }
+        
         static void Main(string[] args)
         {
-            var b = new ContainerBuilder();
-            b.RegisterModule<AttributedMetadataModule>();
-            b.RegisterType<MillenialArtwork>().As<IArtwork>();
-            b.RegisterType<CenturyArtwork>().As<IArtwork>();
-            b.RegisterType<ArtDisplay>().WithAttributeFiltering();
-            using (var c = b.Build())
+            var cb = new ContainerBuilder();
+            cb.RegisterAggregateService<IMyAggregateService>();
+            cb.RegisterAssemblyTypes(typeof(Program).Assembly)
+                .Where(t => t.Name.StartsWith("Class"))
+                .AsImplementedInterfaces();
+            cb.RegisterType<Consumer>();
+
+            using (var container = cb.Build())
             {
-                c.Resolve<ArtDisplay>().Display();
+                var consumer = container.Resolve<Consumer>();
+                //Console.WriteLine(consumer.AllServices.Service3.GetType().Name);
+                Console.WriteLine(consumer.AllServices.GetFourthService("test").GetType().Name);
             }
+            
         }
     }
 }
